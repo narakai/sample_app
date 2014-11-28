f0332b9ce89a5c725aa3e2f7a54730fe0a909ffc
Leon <narakai@126.com>
2014-11-28 18:50:06 +0800

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get 'pages/home'
+
+  get 'pages/contact'
+
+  get 'welcome/index'
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
